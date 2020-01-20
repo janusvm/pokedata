4418ed0c6b8703e86e56c805dba75f38c5714065
Janus Valberg-Madsen <jvalbe13@student.aau.dk>
2020-01-20 16:54:24 +0100

    Finish moves data script

--- data-raw/moves.R
+++ data-raw/moves.R
@@ -1,10 +1,60 @@
 library(tidyverse)
 library(rvest)
 
+
+# Download data ---------------------------------------------------------------
 base_url <- "https://bulbapedia.bulbagarden.net/wiki/"
 
-gen8_moves_html <- read_html(str_c(base_url, "List_of_moves_by_availability_(Generation_VIII)"))
-tms_html        <- read_html(str_c(base_url, "TM"))
-trs_html        <- read_html(str_c(base_url, "TR"))
+gen8_moves_html  <- read_html(str_c(base_url, "List_of_moves_by_availability_(Generation_VIII)"))
+tms_html         <- read_html(str_c(base_url, "TM"))
+trs_html         <- read_html(str_c(base_url, "TR"))
+tutor_moves_html <- read_html(str_c(base_url, "Move_Tutor"))
 
-usethis::use_data("moves")
+
+# Extract tables --------------------------------------------------------------
+moves <-
+  gen8_moves_html %>%
+  html_nodes("table.sortable") %>%
+  .[[2]] %>%
+  html_table() %>%
+  setNames(c("id", "name", "type", "category", "pp", "power", "accuracy", "swsh")) %>%
+  filter(swsh != "") %>%
+  mutate(
+    power    = as.integer(power),
+    accuracy = as.integer(str_sub(accuracy, end = -2))
+  ) %>%
+  select(name:accuracy) %>%
+  as_tibble()
+
+tm_moves <-
+  tms_html %>%
+  html_nodes("table.roundtable") %>%
+  .[[9]] %>%
+  html_table() %>%
+  setNames(c("tmno", "name", "type", "category")) %>%
+  as_tibble() %>%
+  select(tmno, name)
+
+tr_moves <-
+  trs_html %>%
+  html_node("table.roundtable") %>%
+  html_table() %>%
+  setNames(c("trno", "name", "type", "category")) %>%
+  as_tibble() %>%
+  select(trno, name)
+
+tutor_moves <-
+  tutor_moves_html %>%
+  html_nodes("table.sortable") %>%
+  .[[40]] %>%
+  html_table() %>%
+  setNames(c("name", "type", "category", "location")) %>%
+  as_tibble() %>%
+  select(name, location) %>%
+  mutate(location = str_replace(location, " \\(.+\\)", ""))
+
+
+usethis::use_data(moves, overwrite = TRUE)
+usethis::use_data(tm_moves, overwrite = TRUE)
+usethis::use_data(tr_moves, overwrite = TRUE)
+usethis::use_data(tutor_moves, overwrite = TRUE)
