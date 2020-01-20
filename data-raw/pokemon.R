c530bc8b3ee09cb437551d283723c6e7e1edb809
Janus Valberg-Madsen <jvalbe13@student.aau.dk>
2020-01-20 13:42:32 +0100

    Finish pokemon data script

--- data-raw/pokemon.R
+++ data-raw/pokemon.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(rvest)
 
 
+# Download data ---------------------------------------------------------------
 base_url <- "https://bulbapedia.bulbagarden.net/wiki/"
 
 # Load wiki pages
@@ -10,6 +11,8 @@ galar_dex_html  <- read_html(str_c(base_url, "Gdex"))
 abilities_html  <- read_html(str_c(base_url, "List_of_Pokémon_by_Ability"))
 egg_groups_html <- read_html(str_c(base_url, "List_of_Pokémon_by_Egg_Group"))
 
+
+# Extract tables --------------------------------------------------------------
 # Table with base stats
 base_stats_tbl <-
   base_stats_html %>%
@@ -20,9 +23,9 @@ base_stats_tbl <-
   mutate(
     alt  = str_extract(name, "(?<=\\().+(?=\\))"),
     name = str_replace(name, " \\(.+\\)", ""),
-    img  = base_stats_html %>% html_nodes("table.sortable td img") %>% html_attr("src"),
-    id   = basename(img) %>% str_sub(end = -5)
-  )
+    img  = base_stats_html %>% html_nodes("table.sortable td img") %>% html_attr("src")
+  ) %>%
+  select(ndexno, img, name, alt, hp:spe)
 
 # Table with Galar Dex number and types
 galar_dex_tbl <-
@@ -32,11 +35,12 @@ galar_dex_tbl <-
   map(~ html_table(.x) %>%
         setNames(c("gdexno", "ndexno", "img", "name", "type1", "type2")) %>%
         mutate(
-          id     = html_nodes(.x, "img") %>% html_attr("src") %>% basename() %>% str_sub(end = -5),
+          ndexno = as.integer(str_sub(ndexno, 2)),
           gdexno = as.integer(str_sub(gdexno, 2)),
+          img    = html_nodes(.x, "img") %>% html_attr("src"),
           type2  = if_else(type1 != type2, type2, NA_character_)
         ) %>%
-        select(id, gdexno, starts_with("type")) %>%
+        select(ndexno, gdexno, img, starts_with("type")) %>%
         as_tibble()
       ) %>%
   do.call(bind_rows, .)
@@ -47,8 +51,11 @@ abilities_tbl <-
   html_nodes("table.sortable") %>%
   map(~ html_table(.x) %>%
         setNames(c("ndexno", "img", "name", "ability1", "ability2", "ability_hidden")) %>%
-        mutate(id = html_nodes(.x, "img") %>% html_attr("src") %>% basename() %>% str_sub(end = -5)) %>%
-        select(id, starts_with("ability")) %>%
+        mutate(
+          ndexno = as.integer(ndexno),
+          img    = html_nodes(.x, "img") %>% html_attr("src")
+        ) %>%
+        select(ndexno, img, starts_with("ability")) %>%
         as_tibble()
       ) %>%
   do.call(bind_rows, .)
@@ -61,19 +68,71 @@ egg_groups_tbl <-
   setNames(c("ndexno", "img", "name", "egg_group1", "egg_group2")) %>%
   as_tibble() %>%
   mutate(
-    id = egg_groups_html %>% html_nodes("table.sortable img") %>% html_attr("src") %>% basename() %>% str_sub(end = -5)
+    ndexno = as.integer(ndexno),
+    img    = egg_groups_html %>% html_nodes("table.sortable img") %>% html_attr("src")
   ) %>%
-  select(id, starts_with("egg"))
+  select(ndexno, img, starts_with("egg"))
 
-base_stats_tbl %>%
-  right_join(galar_dex_tbl, by = "id") %>%
-  left_join(abilities_tbl, by = "id") %>%
-  left_join(egg_groups_tbl, by = "id") %>%
-  select(id, ndexno, gdexno, name, alt, starts_with("type"), starts_with("egg"), starts_with("ability"), hp:spe, img) %>%
-  ## fill(starts_with("egg")) %>%
-  ## mutate(egg_group2 = if_else(egg_group2 != "", egg_group2, NA_character_)) %>%
-  filter(is.na(img))
 
-# TODO: figure out what to do about alternate forms that only appear in one table
-# TODO: collect tables into one `pokemon` table
-## usethis::use_data("pokemon", overwrite = TRUE)
+# Put it all together -----------------------------------------------------
+miss_forms <- c(
+  "422EMS" = "East Sea",
+  "423EMS" = "East Sea",
+  "550MS"  = "Red-Striped Form",
+  "550BMS" = "Blue-Striped Form",
+  "678MS"  = "Male",
+  "678FMS" = "Female"
+)
+del_forms <- c(
+  "025CoMS",
+  "025OMS",
+  "025HMS",
+  "025SMS",
+  "025KMS",
+  "025AMS",
+  "077MS",
+  "078MS",
+  "083MS",
+  "110MS",
+  "122XYMS",
+  "222MS",
+  "263MS",
+  "264MS",
+  "422MS",
+  "423MS",
+  "554MS",
+  "555MS",
+  "555ZMS",
+  "618MS"
+)
+
+form_join <- function(x, y) {
+  full_join(x, y, by = "img", suffix = c("_x_join", "_y_join")) %>%
+    filter(xor(is.na(ndexno_x_join), is.na(ndexno_y_join)) | (ndexno_x_join == ndexno_y_join)) %>%
+    mutate(ndexno = if_else(is.na(ndexno_x_join), ndexno_y_join, ndexno_x_join)) %>%
+    select(ndexno, everything(), -ends_with("join"))
+}
+
+pokemon <-
+  base_stats_tbl %>%
+  form_join(abilities_tbl) %>%
+  form_join(galar_dex_tbl) %>%
+  form_join(egg_groups_tbl) %>%
+  mutate_if(is.character, ~ if_else(. != "", str_replace(., "\\*", ""), NA_character_)) %>%
+  mutate(id = basename(img) %>% str_sub(end = -5)) %>%
+  select(id, ndexno, gdexno, name, alt, hp:spe, starts_with("ability"), starts_with("type"), starts_with("egg"), img) %>%
+  arrange(ndexno, name) %>%
+  group_by(ndexno) %>%
+  fill(gdexno:name, hp:egg_group2) %>%
+  rowwise() %>%
+  mutate(alt = if_else(is.na(alt), miss_forms[id], alt)) %>%
+  ungroup() %>%
+  filter(
+    !is.na(gdexno),
+    !id %in% del_forms,
+    is.na(alt) | !grepl("Mega|Partner|Alolan", alt)
+  ) %>%
+  arrange(gdexno)
+
+
+usethis::use_data("pokemon", overwrite = TRUE)
